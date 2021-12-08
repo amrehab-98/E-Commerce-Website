7666cec54d64152e9080bad2a606cc2e615f7684
Amr Ehab <amrabdulaziz98@gmail.com>
2021-12-08 22:22:56 +0200

    models changes

--- eCommerce/api/models.py
+++ eCommerce/api/models.py
@@ -3,7 +3,13 @@ from PIL import Image
 
 from django.core.files import File
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import PermissionsMixin
 
 
 # Create your models here.
@@ -57,12 +63,61 @@ class Product(models.Model):
         return thumbnail
 
 
-class MyUser(AbstractUser):
-    not_owned_products = models.ManyToManyField(Product, blank=True)
-    balance = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+class MyUserManager(BaseUserManager):
+	def create_user(self, first_name, last_name, email, username, password=None):
+		if not email:
+			raise ValueError('Users must have an email address')
+		if not username:
+			raise ValueError('Users must have a username')
+
+		user = self.model(
+            first_name=first_name,
+            last_name=last_name,
+			email=self.normalize_email(email),
+			username=username,
+		)
+
+		user.set_password(password)
+		user.save(using=self._db)
+		return user
+
+	def create_superuser(self, email, password, username,first_name=None, last_name=None):
+		user = self.create_user(
+			first_name, last_name, self.normalize_email(email), username, password
+		)
+		user.is_admin = True
+		user.is_staff = True
+		user.is_superuser = True
+		user.save(using=self._db)
+		return user
+
+class MyUser(AbstractBaseUser, PermissionsMixin):
+    first_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+    email=models.EmailField(verbose_name="email", max_length=60, unique=True)
+    username=models.CharField(max_length=30, unique=True)
+    not_owned_products=models.ManyToManyField(Product, blank=True)
+    balance=models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    date_joined=models.DateTimeField(verbose_name="date joined", auto_now_add=True)
+    last_login=models.DateTimeField(verbose_name="last login", auto_now=True)
+    is_admin=models.BooleanField(default=False)
+    is_active=models.BooleanField(default=True)
+    is_staff=models.BooleanField(default=False)
+    is_superuser=models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = MyUserManager()
 
     def __str__(self):
-        return self.first_name + ' ' + self.last_name
+        return self.username
+    
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return True
 
     def get_absolute_url(self):
         return f'/{self.username}/'
@@ -70,8 +125,27 @@ class MyUser(AbstractUser):
     def get_name(self):
         return f'{self.first_name} {self.last_name}'
 
-    def get_unowned_products(self):
+    def get_not_owned_products(self):
         return self.not_owned_products
+
+    def get_balance(self):
+        return self.balance
+
+# class MyUser(AbstractUser):
+#     not_owned_products = models.ManyToManyField(Product, blank=True)
+#     balance = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+
+#     def __str__(self):
+#         return self.first_name + ' ' + self.last_name
+
+#     def get_absolute_url(self):
+#         return f'/{self.username}/'
+
+#     def get_name(self):
+#         return f'{self.first_name} {self.last_name}'
+
+#     def get_unowned_products(self):
+#         return self.not_owned_products
 
 
 class Order(models.Model):
@@ -99,3 +173,8 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return '%s' % self.id
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
