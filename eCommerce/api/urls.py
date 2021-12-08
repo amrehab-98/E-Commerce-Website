a4a2a5614ebfc79fa8f4f27f2f5051764d02bae2
Luai Anwar <55032660+LoayAnwar@users.noreply.github.com>
2021-12-08 22:59:49 +0200

    user endpoint

--- eCommerce/api/urls.py
+++ eCommerce/api/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('products/', views.ProductsList.as_view()),
     url(r'^(?P<username>)[-\w.]+/products/', views.ProductsDetails.as_view()),
     url(r'^users/(?P<username>)[-\w.]+/products/(?P<id>)[0-9]+/', views.ProductsDetails.as_view()),
-    path('users/', views.Users.as_view()),
+    path('register/', views.Users.as_view(), name='register'),
     path('orders/', views.OrdersList.as_view()),
 ]
